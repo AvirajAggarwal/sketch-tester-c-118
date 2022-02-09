f449ab942fa589777523f9b2bc0436b0f6dadbf4
AvirajAggarwal <79792724+AvirajAggarwal@users.noreply.github.com>
2022-02-09 22:17:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 function setup()
 {
     canvas=createCanvas(280,280);
-    canvas.center();
+    canvas.center(); 
+    background("white");
     canvas.mouseReleased(classifyCanvas);
     synth=window.speechSynthesis;
 }
 
 function preload()
 {
-    classifier = ml5.imageClassifier('DodleNet');
+    classifier = ml5.imageClassifier('DoodleNet');
 }
 function clearCanvas()
 {
